646dfdeb877736066da7f1831d8ae93f4391da9c
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2022-07-15 15:48:14 -0400

    Update macAddressChange.py
    Finalized macAddressChange.
    Added to see if the MAC address changed.
    Defined a new function.
    Condition statement to see if the mac address changed.

--- Python/macAddressChange.py
+++ Python/macAddressChange.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
-import subprocess
-import optparse
+import subprocess				# Used for commands
+import optparse					# Used for arguments
+import re						# Used to filter strings Use: pythex.org 		
+
+def get_arguments():
+	parser = optparse.OptionParser()
+	parser.add_option("-i", "--interface", dest = "interface", help = "Interface to change its MAC address")
+	parser.add_option("-m", "--mac", dest = "new_mac", help = "New MAC address")
+	(options, arguments) = parser.parse_args()
+	if not options.interface:
+		parser.error("[-] Please an interface, use --help for more info.")
+	elif not options.new_mac:
+		parser.error("[-] Please an new mac, use --help for more info.")
+	return options
 
 def change_mac(interface, new_mac):
 	print("[+] Changing MAC address for: " + interface + " to " + new_mac)
@@ -9,13 +21,25 @@ def change_mac(interface, new_mac):
 	subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
 	subprocess.call(["ifconfig", interface, "up"])
 
+def get_current_mac(interface):
+	ifconfig_result = str(subprocess.check_output(["ifconfig", interface]))
+	mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
 
-print("[~] Running the command `ifconfig`.")
-subprocess.call("ifconfig", shell = True)
+	if mac_address_search_result:
+		return mac_address_search_result.group(0)
+	else:
+		print("[-] Could not read MAC address.")
 
-parser = optparse.OptionParser()
-parser.add_option("-i", "--interface", dest = "interface", help = "Interface to change its MAC address")
-parser.add_option("-m", "--mac", dest = "new_mac", help = "New MAC address")
-(options, arguments) = parser.parse_args()
 
-change_mac(interface = options.interface, new_mac = options.new_mac)
+options = get_arguments()
+
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
+
+# change_mac(interface = options.interface, new_mac = options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+	print("[+] MAC address was successfully changed to " + current_mac)
+else:
+	print("[-] MAC addresss did not get changed.")
