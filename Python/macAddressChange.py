f970e586327ff58312ee2d88965930ad25546bbd
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2022-07-15 10:45:51 -0400

    Update macAddressChange.py
    Made the code "cleaner"

--- Python/macAddressChange.py
+++ Python/macAddressChange.py
@@ -3,20 +3,19 @@
 import subprocess
 import optparse
 
+def change_mac(interface, new_mac):
+	print("[+] Changing MAC address for: " + interface + " to " + new_mac)
+	subprocess.call(["ifconfig", interface, "down"])
+	subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+	subprocess.call(["ifconfig", interface, "up"])
+
+
 print("[~] Running the command `ifconfig`.")
 subprocess.call("ifconfig", shell = True)
-
 
 parser = optparse.OptionParser()
 parser.add_option("-i", "--interface", dest = "interface", help = "Interface to change its MAC address")
 parser.add_option("-m", "--mac", dest = "new_mac", help = "New MAC address")
 (options, arguments) = parser.parse_args()
 
-interface = options.interface
-new_mac = options.new_mac
-
-print("[+] Changing MAC address for: " + interface + " to " + new_mac)
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether"])
-subprocess.call(["ifconfig", interface, "up"])
+change_mac(interface = options.interface, new_mac = options.new_mac)
