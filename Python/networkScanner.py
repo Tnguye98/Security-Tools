c0fef7ae20ffe8958ef2c60a82eba998e6dbe68c
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2022-07-17 16:37:56 -0400

    Update networkScanner.py
    Created a custom arp packet.

--- Python/networkScanner.py
+++ Python/networkScanner.py
@@ -1,6 +1,13 @@
-#!/user/bin/env python3
+#!/user/bin/env python
 
-import scapy.all as scapy
+# Look into scapy.ls scapy.showb
+
+from scapy.all import arping
 
 def scan(ip):
-	scapy.arping(ip)
+	arp_request = scapy.ARP(pdst = ip)
+	broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")	# ff: is the broadcast
+	arp_request_broadcast = broadcast/arp_request
+	answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout = 1)
+
+scan("10.")
