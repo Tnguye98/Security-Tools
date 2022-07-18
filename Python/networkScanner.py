7d9ae723dab61df9fcb38f0a3d6bacdcc467a9c8
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2022-07-18 15:30:15 -0400

    Update networkScanner.py
    Made the output more pretty.

--- Python/networkScanner.py
+++ Python/networkScanner.py
@@ -1,13 +1,17 @@
 #!/user/bin/env python
 
-# Look into scapy.ls scapy.showb
+# Look into scapy.ls
 
 from scapy.all import arping
 
 def scan(ip):
 	arp_request = scapy.ARP(pdst = ip)
-	broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")	# ff: is the broadcast
-	arp_request_broadcast = broadcast/arp_request
-	answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout = 1)
+	broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")	# ff:.. is the broadcast mac
+	arp_request_broadcast = broadcast / arp_request
+	answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0]
 
-scan("10.")
+	print("IP\t\t\tMAC Address\n--------------------------------------------------")
+	for element in answered_list:
+		print(element[1].psrc + "\t\t" + element[1].hwsrc)
+
+scan("10.10.0.6/24")
