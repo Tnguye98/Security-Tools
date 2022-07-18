2add37510626818a9ebb46bee543f3e7d0e9b141
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2022-07-18 15:53:51 -0400

    Update networkScanner.py
    Created a print function.

--- Python/networkScanner.py
+++ Python/networkScanner.py
@@ -10,8 +10,17 @@ def scan(ip):
 	arp_request_broadcast = broadcast / arp_request
 	answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0]
 
-	print("IP\t\t\tMAC Address\n--------------------------------------------------")
+	clients_list = []
 	for element in answered_list:
-		print(element[1].psrc + "\t\t" + element[1].hwsrc)
+		client_dict = {"ip" : element[1].psrc, "mac" : element[1].hwsrc}
+		clients_list.append(client_dict)
+	return clients_list
 
-scan("10.10.0.6/24")
+def print_result(results_list):
+	print("IP\t\t\tMAC Address\n--------------------------------------------------")
+	for client in results_list:
+		print(client["ip"] + "\t\t" + client["mac"])
+
+
+scan_result = scan("10.10.0.6/24")
+print_result(scan_result)
