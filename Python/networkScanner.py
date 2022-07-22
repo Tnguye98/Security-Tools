72c60fa3ce9285abdee5a34b78e6a2356bc2be47
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2022-07-22 14:37:43 -0400

    Update networkScanner.py
    Changed formatting on variables.

--- Python/networkScanner.py
+++ Python/networkScanner.py
@@ -3,29 +3,29 @@ import scapy.all as scapy
 import optparse
 
 
-def get_arguments():
+def getArguments():
     parser = optparse.OptionParser()
     parser.add_option("-t", "--target", dest="target", help="Target IP / IP range.")
     options, arguments = parser.parse_args()
     return options
 
 def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
+    arpRequest = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    clients_list = []
-    for element in answered_list:
-        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-        clients_list.append(client_dict)
-    return clients_list
+    arpRequestBroadcast = broadcast/arpRequest
+    answeredList = scapy.srp(arpRequestBroadcast, timeout=1, verbose=False)[0]
+    clientsList = []
+    for element in answeredList:
+        clientDict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clientsList.append(client_dict)
+    return clientsList
 
-def print_result(results_list):
+def printResult(results_list):
     print("IP\t\t\tMAC Address\n--------------------------------------------------")
-    for client in results_list:
+    for client in resultsList:
         print(client["ip"] + "\t\t" + client["mac"])
 
 
 options = get_arguments()
-scan_result = scan(options.target)
-print_result(scan_result)
+scanResult = scan(options.target)
+printResult(scanResult)
